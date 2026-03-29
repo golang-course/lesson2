479df789dbb688a8d8b2a60420bbd0f9d8f1baed
august <fshrmn.august@gmail.com>
2026-03-29 13:11:14 +0300

    switch case

--- main.go
+++ main.go
@@ -3,19 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var name string
-	var age int
-	fmt.Print("Введите имя: ")
-	fmt.Scanln(&name)
-	fmt.Print("Введите введите возраст: ")
-	fmt.Scanln(&age)
-	fmt.Printf("Hello, %v, %v \n", name, age)
-	/*
-		if age >= 18 {
-			fmt.Println("Вы совершеннолетний")
-		} else if age <= 0 {
-			fmt.Println("Вы точно обманываете)")
-		} 
-	*/
-
+	var a int
+	a = 3
+	switch {
+	case a > 1:
+		fmt.Println("а > 1")
+		fallthrough
+	case a == 100:
+		fmt.Println("3")
+	case a == 4 || a == 5:
+		fmt.Println("5 or 4")
+	default:
+		fmt.Println("Default case")
+	}
 }
